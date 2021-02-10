61c5fc046453ff863c5dd24ba93d93764186df8d
dannyflatiron <dannyflatiron@gmail.com>
2021-02-10 18:20:28 -0500

    feat: fix CORS problem by setting headers

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const app = express()
 
 app.use(bodyParser.json()) // application/json
 
+// resolve CORS problem
+app.use((request, response, next) => {
+  response.setHeader('Access-Control-Allow-Origin', '*')
+  response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE')
+  response.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  next()
+})
+
 app.use('/feed', feedRoutes)
 
 app.listen(8080)
