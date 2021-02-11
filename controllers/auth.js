13e06427ca6b6e0dec9b8fb28a26d100fef5fb93
dannyflatiron <dannyflatiron@gmail.com>
2021-02-11 02:01:46 -0500

    fix: syntax error line 6. feat: hash password with bcryptjs

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,9 @@
 const User = require('../models/user')
 const { validationResult } = require('express-validator')
+const bcrypt = require('bcryptjs')
 
 exports.signup = (request, response, next) => {
-  const errors = validationResult(req);
+  const errors = validationResult(request);
   if (!errors.isEmpty()) {
     const error = new Error('Validation failed.');
     error.statusCode = 422;
@@ -12,4 +13,22 @@ exports.signup = (request, response, next) => {
   const email = request.body.email
   const name = request.body.name
   const password = request.body.password
+  bcrypt.hash(password, 12)
+  .then(hashedPassword => {
+    const user = new User({
+      email: email,
+      password: hashedPassword,
+      name: name
+    })
+    return user.save()
+  })
+  .then(result => {
+    response.status(201).json({ message: 'User created!', userId: result._id })
+  })
+  .catch(err => {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  });
 }
