1e4b12b448fd3b413845ac34f18efd34a9291fd8
dannyflatiron <dannyflatiron@gmail.com>
2021-02-11 00:33:48 -0500

    feat: enable edit feature. user should be able to edit details of post without having to update every detail

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator')
 const Post = require('../models/post')
+const fs = require('fs')
+const path = require('path')
 
 
 exports.getPosts = (request, response, next) => {
@@ -69,4 +71,58 @@ exports.getPost = (request, response, next) => {
       }
       next(error)
     })
+}
+
+exports.updatePost = (request, response, next) => {
+  const errors = validationResult(request);
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed, entered data is incorrect.');
+    error.statusCode = 422;
+    throw error;
+  }
+
+  const postId = request.params.postId
+  const title = request.body.title
+  const content = request.body.content
+  let imageUrl = request.body.image
+  if (request.file) {
+    imageUrl = request.file.path
+  }
+  console.log('imageUrl', imageUrl)
+
+  if (!imageUrl) {
+    const error = new Error('No file picked.')
+    error.statusCode = 422
+    throw error
+  }
+  Post.findById(postId)
+  .then(post => {
+    if (!post) {
+      const error = new Error('Could not find post.')
+      error.statusCode = 404
+      throw error
+    }
+    if (imageUrl !== post.imageUrl) {
+      clearImage(post.imageUrl)
+    }
+    post.title = title
+    post.imageUrl = imageUrl
+    post.content = content
+    // console.log('editted post', post)
+    return post.save()
+  })
+  .then(result => {
+    response.status(200).json({ message: 'Post updated!', post: result })
+  })
+  .catch(error => {
+    if (!error.statusCode) {
+      error.statusCode = 500
+    }
+    next(error)
+  })
+}
+
+const clearImage = filePath => {
+  filePath = path.join(__dirname, '..', filePath)
+  fs.unlink(filePath, error => console.log(error))
 }
