c12f44796d78b421ebb3fa49e3532a0688f7e10d
dannyflatiron <dannyflatiron@gmail.com>
2021-02-10 23:58:29 -0500

    feat: get post by id and send all necessary post attributes to front end

--- controllers/feed.js
+++ controllers/feed.js
@@ -3,42 +3,9 @@ const Post = require('../models/post')
 
 
 exports.getPosts = (request, response, next) => {
-  response.status(200).json({
-    posts: [{ 
-      title: 'First Post', 
-      content: 'Hello World!', 
-      imageUrl: 'images/skyline.jpg',
-      creator: {
-        name: 'Danny'
-      },
-      createAt: new Date()
-    }]
-  })
-}
-
-exports.createPost = (request, response, next) => {
-  const errors = validationResult(request)
-  if (!errors.isEmpty()) {
-    const error = new Error('Validation failed, entered data is incorrect.')
-    error.statusCode = 422
-    throw error
-  }
-  const title = request.body.title
-  const content = request.body.content
-  const post = new Post({
-    title: title, 
-    content: content,
-    imageUrl: 'images/skyline.jpg',
-    creator: {
-      name: 'Danny'
-    }
-  })
-  post.save()
-  .then(result => {
-    response.status(201).json({
-      message: 'Post created!',
-      post: result
-    })
+  Post.find()
+  .then(posts => {
+    response.status(200).json({ message: 'Fetched psots successfully', posts: posts})
   })
   .catch(error => {
     if (error.statusCode) {
@@ -46,4 +13,60 @@ exports.createPost = (request, response, next) => {
     }
     next(error)
   })
+}
+
+exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed, entered data is incorrect.');
+    error.statusCode = 422;
+    throw error;
+  }
+  if (!req.file) {
+    const error = new Error('No image provided.');
+    error.statusCode = 422;
+    throw error;
+  }
+  const imageUrl = req.file.path;
+  const title = req.body.title;
+  const content = req.body.content;
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: imageUrl,
+    creator: { name: 'Maximilian' }
+  });
+  post
+    .save()
+    .then(result => {
+      res.status(201).json({
+        message: 'Post created successfully!',
+        post: result
+      });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.getPost = (request, response, next) => {
+  const postId = request.params.postId
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error('Could not find post.')
+        error.statusCode = 404
+        throw error
+      }
+      response.status(200).json({ message: 'Post fetched', post: post })
+    })
+    .catch(error => {
+      if (!error.statusCode) {
+        error.statusCode = 500
+      }
+      next(error)
+    })
 }
