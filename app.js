01e136df8e95e2b265ffe30d95773a59f1dd10f7
dannyflatiron <dannyflatiron@gmail.com>
2021-02-10 20:23:31 -0500

    feat: add error middleware and setup middelware to serve images statically

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 require('dotenv').config()
@@ -8,6 +9,8 @@ const feedRoutes = require('./routes/feed')
 const app = express()
 
 app.use(bodyParser.json()) // application/json
+// server images statically
+app.use('/images', express.static(path.join(__dirname, 'images')))
 
 // resolve CORS problem
 app.use((request, response, next) => {
@@ -18,6 +21,13 @@ app.use((request, response, next) => {
 })
 
 app.use('/feed', feedRoutes)
+
+app.use((error, request, response, next) => {
+  console.log(error)
+  const status = error.statusCode || 500
+  const message = error.message
+  response.status(status).json({ message: message })
+})
 
 mongoose.connect(
   `mongodb+srv://dannyreina:${process.env.PASSWORD}@cluster0.vnxsz.mongodb.net/network?retryWrites=true&w=majority`,
