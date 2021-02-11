690bf0d56d7e91ea85476a964917bf451272cfb0
dannyflatiron <dannyflatiron@gmail.com>
2021-02-10 23:57:37 -0500

    feat: use multer to create file directory and file filter

--- app.js
+++ app.js
@@ -3,12 +3,37 @@ const express = require('express')
 const bodyParser = require('body-parser')
 require('dotenv').config()
 const mongoose = require('mongoose')
+const multer = require('multer')
 
 const feedRoutes = require('./routes/feed')
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + '-' + file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
 const app = express()
 
 app.use(bodyParser.json()) // application/json
+// store file and retrieve single files
+app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'))
+
 // server images statically
 app.use('/images', express.static(path.join(__dirname, 'images')))
 
