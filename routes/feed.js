a7477e20159bc1dc453795c2b8b0010c3c62c699
dannyflatiron <dannyflatiron@gmail.com>
2021-02-11 03:12:44 -0500

    feat: add isAuth middleware to every route

--- routes/feed.js
+++ routes/feed.js
@@ -8,7 +8,7 @@ const router = express.Router()
 
 router.get('/posts', isAuth, feedController.getPosts)
 
-router.post('/post', 
+router.post('/post', isAuth,
 // array of middleware validators
   [
     body('title').trim().isLength({min: 5}),
@@ -17,9 +17,9 @@ router.post('/post',
   feedController.createPost
 )
 
-router.get('/post/:postId', feedController.getPost)
+router.get('/post/:postId', isAuth, feedController.getPost)
 
-router.put('/post/:postId', 
+router.put('/post/:postId', isAuth,
 // array of middleware validators
   [
     body('title').trim().isLength({min: 5}),
@@ -27,6 +27,6 @@ router.put('/post/:postId',
   ], 
   feedController.updatePost)
 
-router.delete('/post/:postId', feedController.deletePost)
+router.delete('/post/:postId', isAuth, feedController.deletePost)
 
 module.exports = router
