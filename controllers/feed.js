92503c9d234e3124b1d75d1b9c155aa0451cc59f
dannyflatiron <dannyflatiron@gmail.com>
2021-02-11 21:10:22 -0500

    feat: update all clients whenever a new post is created. show user's name

--- controllers/feed.js
+++ controllers/feed.js
@@ -44,7 +44,6 @@ exports.createPost = async (req, res, next) => {
   const imageUrl = req.file.path;
   const title = req.body.title;
   const content = req.body.content;
-  let creator;
   const post = new Post({
     title: title,
     content: content,
@@ -52,24 +51,26 @@ exports.createPost = async (req, res, next) => {
     creator: req.userId
   });
   try {
-    await post.save()
+    await post.save();
     const user = await User.findById(req.userId);
-      user.posts.push(post);
+    user.posts.push(post);
     await user.save();
-    io.getIO().emit('posts', { action: 'create', post: post })
-      res.status(201).json({
-        message: 'Post created successfully!',
-        post: post,
-        creator: { _id: user._id, name: user.name }
-      });
-  } catch (error) {
+    io.getIO().emit('posts', {
+      action: 'create',
+      post: { ...post._doc, creator: { _id: req.userId, name: user.name } }
+    });
+    res.status(201).json({
+      message: 'Post created successfully!',
+      post: post,
+      creator: { _id: user._id, name: user.name }
+    });
+  } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500;
     }
     next(err);
   }
 };
-
 exports.getPost = async (request, response, next) => {
   const postId = request.params.postId
   const post = await Post.findById(postId)
