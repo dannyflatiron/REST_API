09d00c33c568651ec699b46cf6d454e15abb5fcf
dannyflatiron <dannyflatiron@gmail.com>
2021-02-19 06:20:01 -0500

    feat & fix: send errors to error middleware. fix syntax errors

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -6,16 +6,21 @@ const User = require('../models/user');
 module.exports = {
   createUser: async function({ userInput }, req) {
     //   const email = args.userInput.email;
-    const errors = []
-    if (!validtor.isEmail) {
-      errors.push({ message: 'Email is invalid!'})
+    const errors = [];
+    if (!validator.isEmail(userInput.email)) {
+      errors.push({ message: 'E-Mail is invalid.' });
     }
-    if (validator.isEmpty(userInput.pass) || !validtor.isLength(userInput.password, { min: 5 })) {
-      errors.push({ message: 'Password too short! '})
+    if (
+      validator.isEmpty(userInput.password) ||
+      !validator.isLength(userInput.password, { min: 5 })
+    ) {
+      errors.push({ message: 'Password too short!' });
     }
     if (errors.length > 0) {
-      const error = new Error('Invalid input.')
-      throw error
+      const error = new Error('Invalid input.');
+      error.data = errors;
+      error.code = 422;
+      throw error;
     }
     const existingUser = await User.findOne({ email: userInput.email });
     if (existingUser) {
