e942a81a7117f20773714f296cc2a053b44bbb83
dannyflatiron <dannyflatiron@gmail.com>
2021-02-19 05:53:29 -0500

    fix: syntax error

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const bodyParser = require('body-parser')
 require('dotenv').config()
 const mongoose = require('mongoose')
 const multer = require('multer')
+const { graphqlHTTP } = require('express-graphql');
 
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolver = require('./graphql/resolvers');
 
 
 const fileStorage = multer.diskStorage({
@@ -45,7 +48,14 @@ app.use((request, response, next) => {
   next()
 })
 
-
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: graphqlSchema,
+    rootValue: graphqlResolver,
+    graphiql: true
+  })
+);
 
 app.use((error, request, response, next) => {
   console.log(error)
